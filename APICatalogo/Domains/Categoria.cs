8f87c363252c5eb162a5e529a645622fc02caadc
Diego Niles <diego.niles@hotmail.com>
2023-11-30 22:20:42 -0300

    Criado as migrations e tabelas no banco de Produtos e Categorias

---
+++ APICatalogo/Domains/Categoria.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Domains;
+
+public class Categoria
+{
+
+    public Categoria()
+    {
+        //Boa prática inicializar a coleção
+        Produtos = new Collection<Produto>();    
+    }
+
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+    public ICollection<Produto>? Produtos { get; set; }  //Indica que uma categoria possui vários produtos
+}
